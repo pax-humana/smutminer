44ad73402ff2dd6e72756f25919b63c19f421c7c
Pax Humana <paxhumana@protonmail.com>
2023-11-21 17:24:53 -0800

    Added output verbosity options

--- smutminer.py
+++ smutminer.py
@@ -10,18 +10,22 @@ import shutil
 from PIL import Image
 from io import BytesIO
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # Suppress Tensorflow log messages
-import opennsfw2
-
-NSFW_MODEL_DIR = os.path.dirname(os.path.abspath(__file__))
-
 parser = argparse.ArgumentParser()
 parser.add_argument('command', nargs=1, type=str, choices=['move', 'copy', 'link', 'score'], default='score', help='Action to take, default: Print the image path and NSFW Score to stdout')
 parser.add_argument('-t', '--threshold', type=float, default=.7, help='Default matching threshold for the open_nsfw model (0 - 1). Default: .7')
 parser.add_argument('-l', '--list', action=argparse.BooleanOptionalAction,  help='Print a list of original file paths')
+parser.add_argument('-v', '--verbose', action='count', default=0, help='Output verbosity. Default: Print paths and scores only')
+parser.add_argument('-a', '--all', action=argparse.BooleanOptionalAction,  help='Print scores for every applicable image')
 parser.add_argument('directory', nargs='?', type=str, default='./', help='Input Directory. Default: current working directory.')
 parser.add_argument('output', nargs='?', type=str, default='nsfw', help='Output Subdirectory. Default: nsfw')
 args = parser.parse_args()
+
+VERBOSITY = args.verbose
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(3-VERBOSITY)
+import opennsfw2
+
+NSFW_MODEL_DIR = os.path.dirname(os.path.abspath(__file__))
 
 COMMAND = args.command[0]
 SCORE_THRESHOLD = args.threshold
@@ -36,7 +40,10 @@ def handler(signum, frame):
 def preprocess_and_compute(pimg, model):
     image = opennsfw2.preprocess_image(Image.open(BytesIO(pimg)), opennsfw2.Preprocessing.YAHOO)
     inputs = numpy.expand_dims(image, axis=0)
-    return model.predict(inputs, verbose=0)
+    if VERBOSITY>1:
+        return model.predict(inputs)
+    else:
+        return model.predict(inputs, verbose=0)
 
 def main():
 
@@ -50,18 +57,24 @@ def main():
         filelist = open(os.path.join(OUTPUT_DIR, 'originalpaths.txt'), 'w')
 
     # Pre-load Tensorflow OpenNSFW2 model
-    #print("Loading OpenNSFW2 model")
+    if VERBOSITY>0:
+        print("Loading OpenNSFW2 model")
     model = opennsfw2.make_open_nsfw_model()
 
-    #print("Recursively scanning directory " + INPUT_DIR)
+    if VERBOSITY>0:
+       print("Recursively scanning directory " + INPUT_DIR)
     for fulldir, subdir, files in os.walk(INPUT_DIR):
         subdir[:] = [d for d in subdir if d not in EXCLUDE] # Exclude the output directory
         for filename in files:
             if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.tif', '.bmp', '.gif')):
                 try:
-                    sys.stderr.write("\033[K")
-                    print(os.path.join(fulldir, filename), "scoring...", end='\r', file=sys.stderr)
-                    sys.stderr.flush()
+                    if VERBOSITY==1:
+                        sys.stderr.write("\033[K")
+                        print(os.path.join(fulldir, filename), "scoring...", end='\r', file=sys.stderr)
+                        sys.stderr.flush()
+                    elif VERBOSITY>1:
+                        print(os.path.join(fulldir, filename), "scoring...", file=sys.stderr)
+                        sys.stderr.flush()
 
                     image_data = open(os.path.join(fulldir, filename), 'rb').read()
                     scores = preprocess_and_compute(image_data, model)
@@ -81,12 +94,20 @@ def main():
 
                     sys.stdout.write("\033[K")
                     print(os.path.join(fulldir, filename), "NSFW score:" , scores[0][1])
+                else:
+                    if args.all:
+                        sys.stdout.write("\033[K")
+                        print(os.path.join(fulldir, filename), "NSFW score:" , scores[0][1])
                         
             
             else:
-                sys.stderr.write("\033[K")
-                print(os.path.join(fulldir, filename), "is not a supported image.", end='\r', file=sys.stderr)
-                sys.stderr.flush()
+               if VERBOSITY==1:
+                   sys.stderr.write("\033[K")
+                   print(os.path.join(fulldir, filename), "is not a supported image.", end='\r', file=sys.stderr)
+                   sys.stderr.flush()
+               elif VERBOSITY>1:
+                   print(os.path.join(fulldir, filename), "is not a supported image.", file=sys.stderr)
+                   sys.stderr.flush()
 
     if args.list:
         filelist.close()
